2e34d11478fa4e2cc3408f37076898a6a4041296
Igor Mont <igornathandev@gmail.com>
2022-01-20 15:30:13 -0300

    Create middleware ensure authenticate

---
+++ src/shared/infra/middlewares/ensureAuthenticate.ts
@@ -0,0 +1,29 @@
+import auth from "../../../config/auth";
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { AppError } from "../http/errors/AppError";
+
+interface ITokenPayload {
+  id: string;
+}
+
+async function ensureAuthtenticate(request: Request, response: Response, next: NextFunction) {
+  const { authorization } = request.headers;
+
+  if(!authorization) throw new AppError("Token missing!", 401);
+
+  const token = authorization.replace("Bearer", '').trim();
+
+  try {
+    const data = verify(token, auth.secret_token);
+    const { id } = data as ITokenPayload;
+
+    request.user_id = id;
+
+    return next();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export { ensureAuthtenticate };
